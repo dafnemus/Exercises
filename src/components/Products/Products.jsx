38e8ff336fcfa752adc3f3e67b10b7425b6559bb
dafnemus <dafnemus@gmail.com>
2021-06-25 13:02:57 -0300

    comento mis pruebas fallidas de buscador

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,16 +7,33 @@ import './Products.css';
 
 
 const Products = () => {
-  const { state, addToCart } = useContext(AppContext);
+  const { state, addToCart, } = useContext(AppContext);
   const { products } = state; // TODO: Etapa 1: Filter products by input
-
+// const {pok, setPok} = useState({key: ''})
   const handleAddToCart = product => () => {
     addToCart(product)
   }
 
+  /* Acceder al state del Input:
+  const handleChange = ({ target: { name, value } }) => {
+    setForm((lastForm: any) => ({
+      ...lastForm,
+      [name]: value
+    }));
+  };
+  */
+ /* Filtrar elementos, forma 1
+ {
+    products.filter(product => (
+      <Product key={product.id} product={products} handleAddToCart={handleAddToCart} />
+    ))
+  } */
+  /*
+  recorrer la lista de objetos pokemon, e ir preguntando, si la letra/letras ingresadas estan dentro de ese objeto, e ir agregandolos a una lista aux, la cual se mostrará.
+  */
   return (
     <div className="Products">
-      <AutoCompleteInput /> {/* TODO: Etapa 1: Must create an onSelectItem method */}
+      <AutoCompleteInput /> 
       <div className="Products-items">
         {products.map(product => (
           <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
